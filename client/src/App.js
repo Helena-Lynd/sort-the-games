16c21663b1e9228040b6bdd616bb2b7c3f24f8af
Helena-Lynd <helenalynd@gmail.com>
2023-06-21 16:56:59 -0400

    Updating code to be repeatable

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
 import React, { Fragment, useState }  from 'react';
 import './app-style.css';
 
-function DraggableBox() {
-  return (
-    <div className="text-square">
-      <p>HALO</p>
-    </div>
-  );
+function GameTile(props) {
+	const left = props.left;
+	const height = props.height;
+	
+	return (
+		<div className="game-tile" style={{left: left, top: height }}>
+			<p>{props.title}</p>
+		</div>
+	);
 }
 
 function App() {  
-  return (
-    <Fragment>
-      <div className="title">
-        <span className="title-first-word">SORT</span>
-        <span className="title-second-word">THE</span>
-        <span className="title-third-word">GAMEDLE</span>
-      </div>
+	return (
+		<Fragment>
+			<div className="title">
+				<span className="title-first-word">SORT</span>
+				<span className="title-second-word">THE</span>
+				<span className="title-third-word">GAMEDLE</span>
+			</div>
       
-      <div className="box-wrapper" draggable="true">
-        <DraggableBox />
-      </div>
-    </Fragment>
-  );
+			<div draggable="true" style={{ display: 'flex' }}>
+				<span> <GameTile title='GAME' left='545px' height='40px'/> </span>
+				<span> <GameTile title='GAME' left='565px' height='40px'/> </span>
+				<span> <GameTile title='GAME' left='585px' height='40px'/> </span>
+				<span> <GameTile title='GAME' left='605px' height='40px'/> </span>
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
