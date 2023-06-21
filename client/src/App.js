ec28da3d864c11f3e2d90807338cf52baef65959
Helena-Lynd <helenalynd@gmail.com>
2023-06-21 16:23:23 -0400

    Creating graphics

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Fragment, useState }  from 'react';
 import './app-style.css';
 
-function App() {
-	return (
-		<p>SorttheGamdle</p>
-	)
+function DraggableBox() {
+  return (
+    <div className="text-square">
+      <p>HALO</p>
+    </div>
+  );
+}
+
+function App() {  
+  return (
+    <Fragment>
+      <div className="title">
+        <span className="title-first-word">SORT</span>
+        <span className="title-second-word">THE</span>
+        <span className="title-third-word">GAMEDLE</span>
+      </div>
+      
+      <div className="box-wrapper" draggable="true">
+        <DraggableBox />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
